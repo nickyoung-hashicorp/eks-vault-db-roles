66107a5ce89d7af2e760ee469e730f99fb0ff4c0
nickyoung-hashicorp <nyoung@hashicorp.com>
2022-06-14 15:06:28 -0700

    changes

---
+++ scripts/configure_static_policy.sh
@@ -0,0 +1,11 @@
+cat > product-static.hcl << EOF
+path "database/static-creds/product" {
+  capabilities = ["read"]
+}
+EOF
+vault policy write product-static ./product-static.hcl
+vault write auth/kubernetes/role/product-static \
+    bound_service_account_names=product \
+    bound_service_account_namespaces=default \
+    policies=product-static \
+    ttl=1h
---
+++ scripts/configure_static_role.sh
@@ -0,0 +1,28 @@
+read -p "Enable the database secrets engine"
+vault secrets enable database
+
+read -p "Save and echo the Postgres pod's hostname"
+export POSTGRES_IP=$(kubectl get service -o jsonpath='{.status.loadBalancer.ingress[0].hostname}' \
+   postgres)
+echo $POSTGRES_IP
+
+read -p "Configure the database secrets engine"
+vault write database/config/postgresql \
+    plugin_name=postgresql-database-plugin \
+    allowed_roles="*" \
+    connection_url="postgresql://{{username}}:{{password}}@${POSTGRES_IP}:5432/products?sslmode=disable" \
+    username="postgres" \
+    password="password"
+
+read -p "Create rotation.sql statement"
+cat > rotation.sql << EOF
+ALTER USER "{{name}}" WITH PASSWORD '{{password}}';
+EOF
+
+read -p "Configure static database role with 20s rotation period"
+vault write database/static-roles/product \
+    db_name=products \
+    rotation_statements=@rotation.sql \
+    username="static-vault-user" \
+    rotation_period=20
+
