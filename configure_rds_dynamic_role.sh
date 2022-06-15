76941475b89a015edf69d38eaa1ed61ec6b1975f
nickyoung-hashicorp <nyoung@hashicorp.com>
2022-06-15 11:01:24 -0700

    changes

---
+++ configure_rds_dynamic_role.sh
@@ -0,0 +1,23 @@
+read -p "Enable the database secrets engine[]"
+vault secrets enable database
+
+read -p "Save and echo the Postgres RDS address[]"
+export RDS_ADDR=$(terraform output rds-address)
+echo $RDS_ADDR
+
+read -p "Configure the database secrets engine[]"
+vault write database/config/product \
+    plugin_name=postgresql-database-plugin \
+    allowed_roles="*" \
+    connection_url="postgresql://{{username}}:{{password}}@${RDS_ADDR}:5432/products?sslmode=disable" \
+    username="postgres" \
+    password="password"
+
+read -p "Creaet a role called 'product' with a 20s TTL[]"
+vault write database/roles/product \
+    db_name=product \
+    creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; \
+        GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";" \
+    revocation_statements="ALTER ROLE \"{{name}}\" NOLOGIN;"\
+    default_ttl="20s" \
+    max_ttl="20s"
