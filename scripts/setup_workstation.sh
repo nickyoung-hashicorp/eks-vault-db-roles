0c054e3b051a6af22f01d51969730129509f28ea
nickyoung-hashicorp <nyoung@hashicorp.com>
2022-06-15 21:21:48 -0700

    changes

---
+++ scripts/setup_workstation.sh
@@ -0,0 +1,22 @@
+echo "Install Vault to use the CLI"
+
+export VAULT_VERSION=1.10.3 # Choose your desired Vault version
+wget https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip
+unzip -j vault_*_linux_amd64.zip -d /usr/local/bin
+
+echo "Install awscli"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+echo "Install kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+echo "Install Helm"
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
+chmod 700 get_helm.sh
+./get_helm.sh
+
+echo "Remove files"
+rm -rf aws awscliv2.zip get_helm.sh vault_*_linux_amd64.zip kubectl
