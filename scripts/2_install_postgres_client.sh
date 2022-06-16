b34abac13e6d9d817090deeb9ac4678a37e08b2b
nickyoung-hashicorp <nyoung@hashicorp.com>
2022-06-16 00:23:17 -0700

    changes

---
+++ scripts/2_install_postgres_client.sh
@@ -0,0 +1,5 @@
+# Install PostgreSQL client
+curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
+sudo apt update -y
+sudo apt install postgresql-14 -y
